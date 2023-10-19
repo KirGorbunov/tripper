c0caf25b3e5775314a790529d322691b724da7b2
KirGorbunov <KirGorbunov@gmail.com>
2023-10-19 08:38:37 +0300

    refactor: apply flake8 in urls.py

--- google_trip/urls.py
+++ google_trip/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from .views import *
+from .views import Home, TouristAttractions, Restaurants, Hotels, \
+    ViewAttraction, ViewRestaurant, ViewHotel, Geo, \
+    APITouristAttractionsList, APIRestaurantsList, APIHotelsList
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('tourist-attractions/', TouristAttractions.as_view(), name='tourist_attractions_list'),
+    path('tourist-attractions/', TouristAttractions.as_view(),
+         name='tourist_attractions_list'),
     path('restaurants/', Restaurants.as_view(), name='restaurants_list'),
     path('hotels/', Hotels.as_view(), name='hotels_list'),
-    path('tourist-attractions/<int:pk>/', ViewAttraction.as_view(), name='view_attraction'),
-    path('restaurants/<int:pk>/', ViewRestaurant.as_view(), name='view_restaurant'),
+    path('tourist-attractions/<int:pk>/', ViewAttraction.as_view(),
+         name='view_attraction'),
+    path('restaurants/<int:pk>/', ViewRestaurant.as_view(),
+         name='view_restaurant'),
     path('hotels/<int:pk>/', ViewHotel.as_view(), name='view_hotel'),
     path('geo/', Geo.as_view(), name='geo'),
     path('api/v1/tourist-attractions/', APITouristAttractionsList.as_view()),
