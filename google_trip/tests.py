d84419dc7e759936cdb4a7103376468ac78e2bca
KirGorbunov <KirGorbunov@gmail.com>
2023-11-27 16:25:48 +0300

    feat: add simple unit tests

--- google_trip/tests.py
+++ google_trip/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestGoogleTrip(TestCase):
+
+    def test_tourist_attractions(self):
+        response = self.client.get('/tourist-attractions/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Choose A Country', response.content.decode())
+
+    def test_restaurants(self):
+        response = self.client.get('/restaurants/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Choose A Country', response.content.decode())
+
+    def test_hotels(self):
+        response = self.client.get('/hotels/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Choose A Country', response.content.decode())
